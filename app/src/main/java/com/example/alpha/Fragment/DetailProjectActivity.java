2b1abf1368751e39318e70902fe377ba914b0bae
vinsensiuschristo <hallavinsen@gmail.com>
2021-09-20 14:52:57 +0700

    side menu detailProject

--- app/src/main/java/com/example/alpha/Fragment/DetailProjectActivity.java
+++ app/src/main/java/com/example/alpha/Fragment/DetailProjectActivity.java
@@ -2,8 +2,12 @@ package com.example.alpha.Fragment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.app.Dialog;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +15,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,6 +24,7 @@ import com.example.alpha.R;
 public class DetailProjectActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     Button saveEnum;
+    ImageView imageView;
     String[] arrayPeliculas = {"Project Example 1","Project Example 2","Project Example 3","Project Example 4"};
 
     @Override
@@ -27,30 +33,28 @@ public class DetailProjectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_project);
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,arrayPeliculas);
-
-
     }
 
     //munculin menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_download,menu);
-        return true;
-        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.detail_activity_menu,menu);
+//        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     //onclickItem Menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.item_done:
-                showDialog();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.item_done:
+//                showDialog();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
     //fungsi onClickItem Menu
     void showDialog(){
